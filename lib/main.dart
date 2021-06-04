af1740cfd06a836964190dea435f234e6c4d57dd
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-03 23:29:47 -0300

    Created the graphic and list of transaction

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,20 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Despesas pessoais'),
       ),
-      body: Center(
-        child: Text('Versão inicial'),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de transações'),
+          )
+        ],
       ),
     );
   }
