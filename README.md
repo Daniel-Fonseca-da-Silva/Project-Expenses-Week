c812d8cfdec387d00dcd359277264a70868b5826
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-03 23:22:07 -0300

    Created the first application layout

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project expenses
+
+A new project for IOS and Android application that use Dart language with the framework flutter.
+
+## About project
+
+This project allow the users see your expenses of the week using a percentage and allow the user create your expenses with price and description with label.
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(new ExpensesApp());
+
+class  ExpensesApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas pessoais'),
+      ),
+      body: Center(
+        child: Text('Versão inicial'),
+      ),
+    );
+  }
+}
