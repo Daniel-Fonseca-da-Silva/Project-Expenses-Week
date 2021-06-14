50efe3f634346eedce73ef3c1d5a910715e22970
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-14 01:18:47 -0300

    Created new method to add an element in that list

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,14 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'transaction_form.dart';
 import 'transaction_list.dart';
 import '../models/transaction.dart';
 
 class TransactionUser extends StatefulWidget {
+
   @override
   _TransactionUserState createState() => _TransactionUserState();
 }
 
 class _TransactionUserState extends State<TransactionUser> {
+  
   final _transactions = {
     Transaction(
         id: 't1',
@@ -19,9 +23,28 @@ class _TransactionUserState extends State<TransactionUser> {
         id: 't2', title: 'Conta de luz', value: 58.99, date: DateTime.now())
   };
 
+  // Method
+  _addTransaction(String title, double value) {
+
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now()
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: [TransactionList(_transactions.toList()), TransactionForm()]);
+        children: [TransactionList(_transactions.toList()),
+        TransactionForm()
+        ]
+      );
   }
 }
