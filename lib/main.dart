ead709182d3e17a3d759fa22caaeb1fe9369944e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-06-25 17:41:30 -0300

    Added the method navigator to close modal inside main

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop(); // Close modal
   }
 
   _openTransactionFormModal(BuildContext context) {
